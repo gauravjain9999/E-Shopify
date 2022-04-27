799e7c54469bad105e471970533a1c9b13c4d29c
JAIN Gaurav <gaurav.jain4@soprasteria.com>
2022-04-27 16:47:06 +0530

    gui

--- src/app/my-order/my-order.component.ts
+++ src/app/my-order/my-order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApplicationServiceService } from '../Service/application-service.service';
 import { CartService } from '../Service/cart.service';
+import {MediaChange, MediaObserver} from "@angular/flex-layout";
 
 @Component({
   selector: 'app-my-order',
@@ -11,16 +12,50 @@ import { CartService } from '../Service/cart.service';
 export class MyOrderComponent implements OnInit {
 
   public products : any = [];
+  dashBoardGridCol: number = 2;
   public totalSum : number = 0;
+  flag:boolean;
+  defaultFlag: boolean = true;
+  myWhishList: boolean = false;
 
-  constructor(private applicationService: ApplicationServiceService, private cartService: CartService, private router: Router) { }
+  constructor( private mediaObserver: MediaObserver,  private applicationService: ApplicationServiceService, private cartService: CartService, private router: Router) { }
 
   ngOnInit(): void {
 
     this.cartService.getProduct().subscribe(res=>{
       this.products = res;
       this.totalSum = this.cartService.getTotalPrice();
+    });
+
+    this.mediaObserver.asObservable().subscribe((media: MediaChange[])=>{
+
+      if(media.some(mediaChange => mediaChange.mqAlias="Gt-m")){
+        this.dashBoardGridCol = 2;
+      }
+      else{
+        this.dashBoardGridCol = 2;
+      }
+      console.log(media);
     })
   }
+
+  myOrders(){
+    this.flag = true;
+    this.defaultFlag = false;
+    this.myWhishList = false;
+  }
+
+  myFav(){
+    this.myWhishList = true;
+    this.flag = false;
+    this.defaultFlag = false;
+  }
+
+  removeItem(item: any, index: any){
+    console.log(item);
+    this.cartService.removeCartItem(item, index);
+  }
+
+
 
 }
