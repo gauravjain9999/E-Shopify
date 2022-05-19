fbed759210e6659c9ec0b3bc8ca610308d7795a9
JAIN Gaurav <gaurav.jain4@soprasteria.com>
2022-05-19 13:16:16 +0530

    //Update Code

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, of, retry, take } from 'rxjs';
 
 @Component({
   selector: 'app-page-not-found',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
+  status: boolean = false;
+  textData: any;
+  flag = false;
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  if (navigator.onLine) {
+    this.status = false;
+    // Handle offline error
+    this.textData = 'Connected with Internet'
+  }
+  else {
+    this.status = false;
+    this.textData = 'No Internet Connection'
+  }
+}
+
+
+  refreshPage(){
+
+    this.status = true;
+    if (navigator.onLine) {
+      this.status = true;
+      this.textData = 'Connected with Internet'
+      this.status = false;
+    }
+    else {
+      const abc = interval(1000).pipe(take(6)).subscribe(value =>{
+        if(value < 5){
+          console.log(value);
+          if(navigator.onLine){
+            this.status = false;
+            this.textData = 'Connected with Internet'
+          }
+          else{
+            this.textData = `Attempting to retry - #${value}`
+            this.status = true;
+          }
+        }
+        else{
+          this.textData = 'No Internet Connection'
+        }
+      })
+      console.log("Not Work");
+    }
+  }
 }
