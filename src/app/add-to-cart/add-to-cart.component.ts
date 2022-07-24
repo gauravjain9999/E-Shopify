353bfb9e48956ad5097e7d5ae64f4e274890546b
JAIN Gaurav <gaurav.jain4@soprasteria.com>
2022-07-24 14:04:55 +0530

    //Code Update

--- src/app/add-to-cart/add-to-cart.component.ts
+++ src/app/add-to-cart/add-to-cart.component.ts
@@ -5,46 +5,61 @@ import { DialogDataComponent } from './../Dialog-Box/dialog-data/dialog-data.com
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { CartService } from '../core/Service/cart.service';
-import { Component, OnInit, ViewChild, ElementRef, TemplateRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, TemplateRef, AfterViewInit } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-add-to-cart',
   templateUrl: './add-to-cart.component.html',
   styleUrls: ['./add-to-cart.component.css']
 })
-export class AddToCartComponent implements OnInit {
+export class AddToCartComponent implements OnInit, AfterViewInit {
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   public products : any = [];
+  pageLength:any;
   public totalSum : number = 0;
   isChecked : boolean;
   sortColumn: string;
   sortDirection: 'asc' | 'desc';
   found : any;
+  matFlag: boolean = false;
   dataSource : MatTableDataSource<any>;
   showFlagSpinner: boolean = true;
   displayedColumns: any = ['title', 'productImage', 'description', 'price', 'quantity', 'total', 'remove'];
 
-  constructor(private applicationService: ApplicationServiceService, private dialog: MatDialog, private cartService: CartService, private router: Router) { }
+  constructor(private applicationService: ApplicationServiceService, 
+    private dialog: MatDialog, private cartService: CartService, private router: Router) { 
 
-  ngOnInit(): void {
-
+      setTimeout(() =>{
+        this.showFlagSpinner = false;
+      },3000);
+  
+      this.showFlagSpinner = true;
+      this.cartService.getProduct().subscribe(res=>{
+      this.products = res;
+      this.dataSource = new MatTableDataSource<any>(this.products);
+      sessionStorage.setItem('DATA_SOURCE',  JSON.stringify(this.products));
+      this.totalSum = this.cartService.getTotalPrice();
+      if(this.products.length === 0){
+        sessionStorage.clear();
+      }  
+    })
+}
+  
+  ngAfterViewInit(): void {
     setTimeout(() =>{
-      this.showFlagSpinner = false;
-    },3000);
-
-    this.showFlagSpinner = true;
-    this.cartService.getProduct().subscribe(res=>{
-    this.products = res;
-    this.dataSource = new MatTableDataSource<any>(this.products);
-    sessionStorage.setItem('DATA_SOURCE',  JSON.stringify(this.products));
-    this.totalSum = this.cartService.getTotalPrice();
-    if(this.products.length === 0){
-      sessionStorage.clear();
-    }  
-  })
+      this.matFlag = true;
+      this.dataSource.paginator = this.paginator;
+      this.pageLength = this.dataSource.filteredData.length;
+    },2000);
+  }
+  
+  ngOnInit(): void {
+    this.setPagination();
   }
 
   sortChange(event: any){
@@ -53,6 +68,13 @@ export class AddToCartComponent implements OnInit {
     this.sortDirection = event.direction;
     console.log(this.sortColumn);
     console.log(this.sortDirection);
+  }
+
+
+  setPagination(){
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.pageLength = this.dataSource.filteredData.length;
   }
 
 
